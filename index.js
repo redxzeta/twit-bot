3730c1046c2ae69331325cf653b50699bf6efde0
redxzeta <xzetanzero@gmail.com>
2022-02-01 21:24:37 -0800

    delay

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ const INTERVAL = Number(process.env.INTERVAL);
 
 const SOLUTION = Number(process.env.SOLUTION);
 
+const MIN = 10;
+
+const MAX = 39;
+
+const MINUTE = 60000;
+
+let runTimes = 0;
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
+function getRandomArbitrary() {
+  return Math.random() * (MAX - MIN) + MIN;
+}
+
 const userClient = new TwitterApi({
   appKey: process.env.CONSUMER_KEY,
   appSecret: process.env.CONSUMER_SECRET,
@@ -24,12 +41,20 @@ const funStuff = async () => {
   await Promise.all(
     tweets.data.data.map(async (tweet) => {
       await userClient.v2.like(CURRENT_USER_ID, tweet.id);
+      const delaySeconds = getRandomArbitrary() * 1000;
+      delay(delaySeconds);
       await userClient.v2.retweet(CURRENT_USER_ID, tweet.id);
-      console.log("success");
+      const delayMinute =
+        getRandomArbitrary() % 2 === 0
+          ? MINUTE + delaySeconds
+          : MINUTE - delaySeconds;
+      delay(delayMinute);
     })
   );
 };
-console.log("running");
+console.log("running the fun stuff");
 setInterval(() => {
   funStuff();
+  runTimes += 1;
+  console.log(`Ran ${runTimes} times`);
 }, INTERVAL);
